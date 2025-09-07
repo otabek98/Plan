e80f67d91d85d876f557c77810e721db3c6a8059
Otabek <atabayev070@gmail.com>
2025-09-07 18:14:20 +0900

    PORTNI togirladim

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 });
 
 const server = http.createServer(app);
-let PORT = 3000;
+let PORT = 5000;
 server.listen(PORT, function () {
     console.log(`The server is running succesfully on port: ${PORT}`);
 });
