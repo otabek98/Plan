b4b09cedf285187fff8493f57862d12708fe9ab6
Otabek <atabayev070@gmail.com>
2025-09-07 18:06:01 +0900

    ejsda portfolioni qismi bajarildi

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-console.log("Web serverni boshlash");
+console.log('Web serverni boshlash');
 
-const express = require("express");
+const { log } = require('async');
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 
 // 1 kirish qismi
-app.use(express.static("public")); // bu har qanday kirib keladigan malumot uchun public qismi ochiq degan manoni bildiradi
+app.use(express.static('public')); // bu har qanday kirib keladigan malumot uchun public qismi ochiq degan manoni bildiradi
 app.use(express.json()); // kirib kelayotgan json malumotlarni Object ga ogirib beradi
 app.use(express.urlencoded({ extended: true })); //form dan biror bir malumotni post qilsak express qabil qilib oladi
 
 // 2 Session code
 // 3 backend da html ni yasab uni client ga yuboramiz
-app.set("views", "views"); // folder ni nomi
-app.set("view engine", "ejs");
+app.set('views', 'views'); // folder ni nomi
+app.set('view engine', 'ejs');
 
 // 4 routing codelari
-app.get("/home", function (req, res) {
-  res.end(`<h1 style="background: red">Hello World by Otabek </h1>`);
+app.post('/create-item', (req, res) => {
+    console.log(req.body);
+    res.json({ test: 'succes' });
 });
-app.get("/about", function (req, res) {
-  res.end(`<h1 style="background: red">Siz About bolimidasiz</h1>`);
+app.get('/', function (req, res) {
+    res.render('harid');
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`The server is running succesfully on port: ${PORT}`);
+    console.log(`The server is running succesfully on port: ${PORT}`);
 });
